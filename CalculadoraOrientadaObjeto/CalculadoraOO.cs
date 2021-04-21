27bf93c9159d507ad7d1b77c3ed5e68c7c4f96b2
“PedroSRN” <Seu>
2021-04-21 17:25:41 -0300

    primeiro commit

---
+++ CalculadoraOrientadaObjeto/CalculadoraOO.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraOrientadaObjeto
+{
+    public class CalculadoraOO
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string opcao = "";
+                Operacoes operacoes = new Operacoes();
+                operacoes.MostrarMenu();
+                opcao = Console.ReadLine();
+                operacoes.ObterNumeros();
+               
+                //double resultado = 0;
+
+                switch (opcao)
+                {
+                    case "1": operacoes.soma(); break;
+                    case "2": operacoes.subtracao(); break;
+                    case "3": operacoes.multiplicacao(); break;
+                    case "4": operacoes.divisao(); break;
+                   
+                    default:
+                        Environment.Exit(1);
+                        break;
+                }
+
+
+            }
+        }
+    }
+
+    public class Operacoes : MENU
+    {
+        public double primeiroNumero, segundoNumero;
+        public string[] operacoesRealizadas = new string[100];
+        int contadorOperacoesRealizadas = 0;
+        public void ObterNumeros()
+        {
+            Console.WriteLine("Digite o Primeiro Valor: ");
+            primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o Segundo Valor: ");
+            segundoNumero = Convert.ToDouble(Console.ReadLine());
+            
+            contadorOperacoesRealizadas++;
+
+        }
+        public void soma()
+        {
+            string simboloOperacao = "+";
+            double resultado = primeiroNumero + segundoNumero;
+            string conta = $"{primeiroNumero} {simboloOperacao } {segundoNumero} = {resultado}";
+            Console.WriteLine(conta);
+            Console.WriteLine();
+        }
+         
+        public void subtracao()
+        {
+            string simboloOperacao = "-";
+            double resultado = primeiroNumero - segundoNumero;
+            string conta = $"{primeiroNumero} {simboloOperacao } {segundoNumero} = {resultado}";
+            Console.WriteLine(conta);
+            Console.WriteLine();
+        }
+
+        public void multiplicacao()
+        {
+            string simboloOperacao = "*";
+            double resultado = primeiroNumero * segundoNumero;
+            string conta = $"{primeiroNumero} {simboloOperacao } {segundoNumero} = {resultado}";
+            Console.WriteLine(conta);
+            Console.WriteLine();
+        }
+
+        public void divisao()
+        {
+            if (segundoNumero == 0)
+            {
+                Console.WriteLine("Segundo número inválido! Tente novamente");
+
+            }
+            else if(segundoNumero!= 0)
+                    { string simboloOperacao = "/";
+                double resultado = primeiroNumero / segundoNumero;
+                string conta = $"{primeiroNumero} {simboloOperacao } {segundoNumero} = {resultado}";
+                Console.WriteLine(conta);
+                Console.WriteLine();
+            }
+        }
+
+       
+       
+       
+    }
+}
+ 
---
+++ CalculadoraOrientadaObjeto/MENU.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CalculadoraOrientadaObjeto
+{
+    public class MENU
+    {
+        public void MostrarMenu()
+        {
+
+            Console.WriteLine();
+
+            Console.WriteLine("Calculadora Tabajara 1.7.1");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Digite 1 para somar");
+
+            Console.WriteLine("Digite 2 para subtrair");
+
+            Console.WriteLine("Digite 3 para multiplicação");
+
+            Console.WriteLine("Digite 4 para divisão");
+
+            Console.WriteLine("Digite 5 para visualizar as operações realizadas");
+
+            Console.WriteLine("Digite S para sair");
+
+
+
+        }
+    }
+}
